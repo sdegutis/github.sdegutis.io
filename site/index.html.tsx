1dcd8e2e82383511a69808f91c9a30ea2958fe85
sdegutis <sbdegutis@gmail.com>
2024-08-21 18:20:25 -0500

    Article.

--- site/index.html.tsx
+++ site/index.html.tsx
@@ -3,7 +3,7 @@ import { Page } from "./page.js";
 
 export default <Page title="Blog">
   <ul>
-    {articles.map(article => <>
+    {articles.toReversed().map(article => <>
       <li>{article.date} - <a href={article.path}>{article.title}</a></li>
     </>)}
   </ul>
